18c233c8ea321a3f1a8b7090f92ca747dce50f7d
kannan <kannan.mumbai@yahoo.com>
2023-07-16 22:10:19 +0530

    first app

---
+++ WelcomeKoenigApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WelcomeKoenigApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
